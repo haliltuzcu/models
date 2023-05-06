3bed0ab3b98b53141e458e8ab13a12d74235ba80
haliltuzcu <007canberk@gmail.com>
2023-05-06 13:54:33 +0300

    studenteklendi

--- fscohort/urls.py
+++ fscohort/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import fscohort,goodbye
 
 urlpatterns = [
-    
+    path('', fscohort),
+    path('goodbye/', goodbye)
 ]
--- fscohort/views.py
+++ fscohort/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def fscohort(request):
+    return HttpResponse('''
+    <h2> welcome to fscohort </h2>
+    ''')
+def goodbye(request):
+    return HttpResponse('''
+    <h2> goodbye </h2>
+    ''')
